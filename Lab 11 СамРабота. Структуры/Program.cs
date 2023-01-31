3d93f6432c4e8da8cfe173d97fd2fec8e2686199
Maris Shapiro <19maris72@gmail.com>
2023-01-31 20:56:42 +0300

    Добавьте файлы проекта.

---
+++ Lab 11 СамРабота. Структуры/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_СамРабота._Структуры
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearEquation eq = new LinearEquation { k = 2, b =  };
+            double root = eq.Root();
+            Console.WriteLine("Root: " + root);            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab 11 СамРабота. Структуры/linear equation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_11_СамРабота._Структуры
+{
+    struct LinearEquation
+    {
+        public double k;
+        public double b;
+
+        public double Root()
+        {
+            return -b / k;
+        }
+    }
+}   
